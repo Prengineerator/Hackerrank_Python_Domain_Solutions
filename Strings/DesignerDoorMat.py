9912c60975fffc625b9f6b03ce2ce0d8fcff7fcd
Prengineerator <47515422+Prengineerator@users.noreply.github.com>
2020-10-01 16:22:38 +0530

    Update DesignerDoorMat.py
    New Updated Optimised Solution of the Same Problem Has been Updated

--- Strings/DesignerDoorMat.py
+++ Strings/DesignerDoorMat.py
@@ -2,13 +2,10 @@
 Title     : Designer Door Mat
 Subdomain : Strings
 Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
+Author    : Ayush Garg
+Created   : 01 October 2020
 Problem   : https://www.hackerrank.com/challenges/designer-door-mat/problem
 '''
-N, M = map(int,input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
-for i in range(1,N,2): 
-    print(int((M-3*i)/2)*'-'+(i*'.|.')+int((M-3*i)/2)*'-')
-print(int((M-7)/2)*'-'+'WELCOME'+int((M-7)/2)*'-')
-for i in range(N-2,-1,-2): 
-    print(int((M-3*i)/2)*'-'+(i*'.|.')+int((M-3*i)/2)*'-')
+n, m = map(int,input().split())
+p = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(p + ['WELCOME'.center(m, '-')] + p[::-1]))
