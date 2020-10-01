226680ba450065c7610057c2f38c6bcac65ae735
Prengineerator <47515422+Prengineerator@users.noreply.github.com>
2020-10-01 15:53:31 +0530

    Update PythonIfElse.py
    Beautify Code and Add Python3 Solution

--- Introduction/PythonIfElse.py
+++ Introduction/PythonIfElse.py
@@ -2,24 +2,19 @@
 Title     : Python If-Else
 Subdomain : Introduction
 Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
+Author    : Ayush Garg
+Created   : 01 October 2020
 Problem   : https://www.hackerrank.com/challenges/py-if-else/problem
 '''
 #!/bin/python3
 
-import sys
-
-
-N = int(input().strip())
-n= N
-w = 'Weird'
-nw = 'Not Weird'
-if n % 2 == 1:
-    print(w)
-elif n % 2 == 0 and (n>=2 and n<5):
-    print(nw)
-elif n % 2 == 0 and (n>=6 and n<=20):
-    print(w)
-elif n % 2 == 0 and (n>20):
-    print(nw)    
+N = int(input())
+if N%2!=0:
+    print("Weird")
+else :
+    if 2<N<5:
+        print("Not Weird")
+    if 6<N<=20:
+        print("Weird")
+    if N>20:
+        print("Not Weird")    
