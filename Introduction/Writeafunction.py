dae3323bb9aa26453ed7639eb536971678cb7a2d
Prengineerator <47515422+Prengineerator@users.noreply.github.com>
2020-10-01 15:44:47 +0530

    Update Writeafunction.py

--- Introduction/Writeafunction.py
+++ Introduction/Writeafunction.py
@@ -2,14 +2,12 @@
 Title     : Write a function
 Subdomain : Introduction
 Domain    : Python
-Author    : Ahmedur Rahman Shovon
-Created   : 15 July 2016
+Author    : Ayush Garg
+Created   : 01 October 2020
 Problem   : https://www.hackerrank.com/challenges/write-a-function/problem
 '''
 def is_leap(year):
     leap = False
-    if (year % 400 == 0):
+    if(year%4==0 and year%100!=0 or year%400==0):
         leap = True
-    elif year % 4 == 0 and year % 100 !=0:
-        leap = True    
     return leap
