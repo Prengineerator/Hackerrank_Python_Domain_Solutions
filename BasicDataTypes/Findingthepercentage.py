ea3afdda30a426b3b4ae8d249a3d3717855de68d
Prengineerator <47515422+Prengineerator@users.noreply.github.com>
2020-10-01 16:09:47 +0530

    Update Findingthepercentage.py
    This Code is Against the New Pre-Formatted File of the Hackerrank.
    So it needs to be Updated...

--- BasicDataTypes/Findingthepercentage.py
+++ BasicDataTypes/Findingthepercentage.py
@@ -7,16 +7,17 @@ Created   : 15 July 2016
 Problem   : https://www.hackerrank.com/challenges/finding-the-percentage/problem
 '''
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-n=int(input())
-ar={}
-for i in range(0,n):
-    s=input()
-    ss=s.split(" ")
-    n=ss[0]
-    m1=float(ss[1])
-    m2=float(ss[2])
-    m3=float(ss[3])
-    m_avg=(m1+m2+m3)/3.0
-    ar[n]="%.2f" % m_avg
-s_name=input()
-print(ar[s_name])
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+m=len(student_marks[query_name])
+sum1=00.00
+for i in range(0,m):
+    sum1=student_marks[query_name][i]+sum1
+print("{:.2f}".format(sum1/m));
+
